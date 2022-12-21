dc005a05e62f00b81a3f410288b59d169427feab
agry88 <rgok307085@gmail.com>
2022-12-21 11:50:35 +0800

    Feat: Add ActiveColor,inActiveColor,Icon for tab navigator.

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,6 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  tabNavigator: {
+    activeTintColor: '#66A8A6',
+    inactiveTintColor: '#9c9c9cab',
+  }
 };
--- navigation/MainStackNavigator.tsx
+++ navigation/MainStackNavigator.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Ionicons, MaterialCommunityIcons, AntDesign, FontAwesome } from '@expo/vector-icons';
 import { MainStackParams } from '../types';
 import HomeScreen from '../screens/HomeScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -6,26 +7,56 @@ import FindScreen from '../screens/FindScreen';
 import AddCardSetScreen from '../screens/AddCardSetScreen';
 import AnalyzeScreen from '../screens/AnalyzeScreen';
 import SettingScreen from '../screens/SettingScreen';
+import Color from "../constants/Colors";
+
 
 const MainStack = createBottomTabNavigator<MainStackParams>();
 
 export default function MainStackNavigator() {
     return (
-        <MainStack.Navigator initialRouteName='Home'>
+        <MainStack.Navigator
+            screenOptions={{
+                tabBarInactiveTintColor: Color.tabNavigator.inactiveTintColor,
+                tabBarActiveTintColor: Color.tabNavigator.activeTintColor,
+                headerShown: false,
+                tabBarShowLabel: false,
+            }}
+            initialRouteName='Home'
+        >
             <MainStack.Screen name="Home" component={HomeScreen}
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" color={color} size={size} />
+                    ),
+                }}
             />
             <MainStack.Screen name="Find" component={FindScreen}
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="folder-search" color={color} size={size} />
+                    ),
+                }}
             />
             <MainStack.Screen name="AddCardSet" component={AddCardSetScreen}
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="pluscircle" color={Color.tabNavigator.activeTintColor} size={size} />
+                    ),
+                }}
             />
             <MainStack.Screen name="Analyze" component={AnalyzeScreen}
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="bar-chart-o" color={color} size={size} />
+                    ),
+                }}
             />
             <MainStack.Screen name="Setting" component={SettingScreen}
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="setting" color={color} size={size} />
+                    ),
+                }}
             />
         </MainStack.Navigator>
     );
