2dd3af9454bdc67390a07fbd414488352edb5ecf
agry88 <rgok307085@gmail.com>
2022-12-25 22:26:35 +0800

    Feat: add search history to asyncstorage

--- screens/FindScreen.tsx
+++ screens/FindScreen.tsx
@@ -1,18 +1,62 @@
-import * as React from 'react';
-import { StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { useEffect, useState } from 'react';
+import { StyleSheet, TextInput, TouchableWithoutFeedback } from "react-native";
 import styled from 'styled-components/native';
 import StudiCardTitle from '../components/atoms/ScreenTitle';
 import { Entypo } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function FindScreen() {
+
+    const [search, setSearch] = useState<string>()
+    const [searchHistory, setSearchHistory] = useState<string[]>([])
+
+
+    // Get search history
+    useEffect(() => {
+        const getSearchHistory = async () => {
+            try {
+                const history = await AsyncStorage.getItem('@search_history')
+                const c = history ? JSON.parse(history) : []
+                setSearchHistory(c)
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getSearchHistory()
+    }, [])
+
+
+    const handleSearch = async () => {
+        try {
+            if (!search) return
+            if(searchHistory?.includes(search)) return
+            const history = await AsyncStorage.getItem('@search_history')
+            const c = history ? JSON.parse(history) : []
+            c.push(search)
+            await AsyncStorage.setItem('@search_history', JSON.stringify(c));
+            setSearchHistory(prev => [...prev, search])
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const handleRemoveSearchHistory = async (history: string) => {
+        try {
+            const c = searchHistory?.filter(h => h !== history)
+            await AsyncStorage.setItem('@search_history', JSON.stringify(c));
+            setSearchHistory(prev => prev?.filter(h => h !== history))
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     return (
         <FindScreenContainer>
             <StudiCardTitle text="StudiCard" />
             <InputContainer>
-                <Input onBlur={() => console.log("searching in blur")} />
-                <TouchableWithoutFeedback onPress={() => console.log("searching")}>
+                <Input onBlur={handleSearch} onChangeText={text => setSearch(text)} />
+                <TouchableWithoutFeedback onPress={handleSearch}>
                     <Entypo name="magnifying-glass" style={styles.icon} size={24} color="black" />
                 </TouchableWithoutFeedback>
             </InputContainer>
@@ -21,15 +65,27 @@ export default function FindScreen() {
             </RecordTitle>
             <RecordContainer>
 
-                <RecordItem>
-                    <AntDesign name="clockcircleo" size={24} color="black" />
-                    <TouchableWithoutFeedback>
-                        <RecordItemText>
-                            搜尋紀錄1
-                        </RecordItemText>
-                    </TouchableWithoutFeedback>
-                </RecordItem>
-                
+                {searchHistory?.map((history,index) => {
+                    return (
+                        <RecordItem key={index}>
+                            <RecordItemTextAndIconContainer>
+                                <AntDesign name="clockcircleo" size={24} color="black" />
+                                <TouchableWithoutFeedback>
+                                    <RecordItemText>
+                                        {history}
+                                    </RecordItemText>
+                                </TouchableWithoutFeedback>
+                            </RecordItemTextAndIconContainer>
+
+                            <RecordItemCloseIconContainer>
+                                <TouchableWithoutFeedback onPress={() => handleRemoveSearchHistory(history)}>
+                                    <AntDesign name="close" size={24} color="black" />
+                                </TouchableWithoutFeedback>
+                            </RecordItemCloseIconContainer>
+                        </RecordItem>
+                    )
+                })}
+
             </RecordContainer>
         </FindScreenContainer>
     );
@@ -66,6 +122,7 @@ const RecordTitle = styled.Text`
 
 const RecordItem = styled.View`
     flex-direction: row;
+    justify-content: space-between;
     align-items: center;
     margin: 10px 0px;
 `
@@ -82,6 +139,15 @@ const RecordContainer = styled.View`
     margin-top: 10px;
     padding: 0px 10px;
     width: 100%;
+`
+
+const RecordItemTextAndIconContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+const RecordItemCloseIconContainer = styled.View`
+
 `
 
 const styles = StyleSheet.create({
